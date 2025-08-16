08ebbe708bdaa5db6fa99f2c00695f478840ed21
shao <68085490+shaoming11@users.noreply.github.com>
2025-08-15 20:07:51 -0400

    ex card

--- src/pages/ExPage/ExPage.tsx
+++ src/pages/ExPage/ExPage.tsx
@@ -1,18 +1,39 @@
 import Header from "@/components/Header"
 import { useState } from "react";
+import { GoArrowUpRight } from "react-icons/go";
+import roboticsImage from "@/assets/robotics.jpg"
 
 function ExPage() {
-    const [card, setCard] = useState(false);
+    const [card, setCard] = useState(true);
 
     return (
         <>
             <Header/>
-            <h1 className="">Experiences</h1>
-            <div>
-                <h2>Co-Founder @ Altura Robotics</h2>
-                <h2>May 2024-Present</h2>
-                <img src=""></img>
-                <p className={card ? "inline" : "hidden"}>Hosted tournament of 24 teams. Developed 100+ autonomous routes. Hosted 100+ workshops</p>
+            <h1 className="text-5xl mb-5">Experiences</h1>
+            <div className="bg-gray-100 w-full p-5 rounded-lg flex">
+                <div className={card ? "hidden" : "block"}>
+                    <h1 className="flex-grow">Co-Founder @ Altura Robotics</h1>
+                    <h2>May 2024 - Present</h2>
+                    <img src=""></img>
+                </div>
+                <div className={card ? "block" : "hidden"}>
+                    <div className="flex">
+                        <div className="flex-grow">
+                            <h1 className="text-3xl">Co-Founder</h1>
+                            <h2 className="text-l">Altura Robotics</h2>
+                        </div>
+                        <h2 className="text-l">May 2024 - Present</h2>
+                    </div>
+                    <div className="bg-gray-300 p-5 rounded-lg mt-5">
+                        <div className="relative">
+                            <img src={roboticsImage} alt="jamal" className="w-full h-64 object-cover" />
+                            <div className="absolute inset-0 flex items-center justify-center">
+                            </div>
+                        </div>
+                        <p className="my-5">Hosted tournament of 24 teams. Developed 100+ autonomous routes. Hosted 100+ workshops</p>
+                        <a target="_blank" href="https://www.instagram.com" className="bg-gray-400 p-2 hover:bg-gray-500">Website <GoArrowUpRight className="inline"/></a>
+                    </div>
+                </div>
             </div>
         </>
     )
